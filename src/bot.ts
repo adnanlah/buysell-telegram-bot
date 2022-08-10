c34803a615d7b0972117823462551de9ebffbaee
Adnan Lahrech <lahadn@gmail.com>
2022-08-10 01:14:49 +0100

    fixing port error

---
+++ src/bot.ts
@@ -0,0 +1,95 @@
+import { Bot } from "grammy"
+import { GrammyError, HttpError } from "grammy"
+import { sleep, isCorrectOffer, isIncludesTrust, isIncludesReserve } from "./helpers"
+import { usernameRule, formatRule, askTrustRule, botReplyGenerator, reserveRule } from "./dialog"
+import { rulesType, ruleType } from "./types"
+
+if (process.env.BOT_TOKEN == null) throw Error("BOT_TOKEN is missing.")
+export const bot = new Bot(`${process.env.BOT_TOKEN}`, {
+  botInfo: {
+    id: 5556548689,
+    is_bot: true,
+    first_name: "BuySell Monitor",
+    username: "buyselldz_bot",
+    can_join_groups: true,
+    can_read_all_group_messages: false,
+    supports_inline_queries: false
+  }
+})
+
+bot.on("message:text", async (ctx) => {
+  try {
+    const rulesBroken: rulesType = {
+      username: { value: false, content: usernameRule },
+      askTrust: { value: false, content: askTrustRule },
+      reserve: { value: false, content: reserveRule },
+      format: { value: false, content: formatRule }
+    }
+
+    const text: string = ctx.message.text
+    const isOrphan: boolean = ctx.message.reply_to_message === undefined
+
+    const user = await ctx.getAuthor()
+
+    if (
+      ctx.from?.is_bot ||
+      ctx.from?.username === "GroupAnonymousBot" ||
+      user?.status === "creator"
+    ) {
+      return
+    }
+
+    if (ctx.from.username === undefined) {
+      rulesBroken["username"].value = true
+    }
+
+    if (isOrphan && !isCorrectOffer(text) && isIncludesReserve(text)) {
+      rulesBroken["reserve"].value = true
+    }
+
+    if (isOrphan && !isCorrectOffer(text)) {
+      rulesBroken["format"].value = true
+    }
+
+    if (isIncludesTrust(text) && !isIncludesReserve(text)) {
+      rulesBroken["askTrust"].value = true
+    }
+
+    const rulesBrokenFiltered: ruleType[] = Object.values(rulesBroken).filter(
+      (rule) => rule.value === true
+    )
+
+    if (rulesBrokenFiltered.length > 0) {
+      const botReplyContent = botReplyGenerator(rulesBrokenFiltered)
+      const botReplyMessage = await ctx.reply(botReplyContent, {
+        reply_to_message_id: ctx.msg.message_id,
+        parse_mode: "HTML",
+        disable_notification: true,
+        allow_sending_without_reply: false,
+        protect_content: true,
+        disable_web_page_preview: true
+      })
+
+      await sleep(3000 + rulesBrokenFiltered.length * 2000)
+      await bot.api.deleteMessage(ctx.msg.chat.id, botReplyMessage.message_id)
+      await bot.api.deleteMessage(ctx.msg.chat.id, ctx.msg.message_id)
+    }
+  } catch (err: any) {
+    throw new Error(err)
+  }
+})
+
+bot.catch((err) => {
+  const ctx = err.ctx
+  console.error(`Error while handling update ${ctx.update.update_id}:`)
+  const e = err.error
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description)
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e)
+  } else {
+    console.error("Unknown error:", e)
+  }
+})
+
+export default bot
---
+++ src/dialog.ts
@@ -0,0 +1,25 @@
+import { rulesType, ruleType } from "./types"
+export const botReplyGenerator = (rulesBrokenFiltered: ruleType[]): string => {
+  return `‼️ سوف يتم حذف رسالتك بعد ثواني لانك خالفت (${
+    rulesBrokenFiltered.length
+  }) القوانين التالية:
+  
+  ${rulesBrokenFiltered.map((rule) => rule.content).join("\n\n")}
+  
+  <i>- لأي استفسارات انظم لمجموعة التوثيق والنقاشات: https://t.me/+1dS6r91r0hYzZWZk</i>`
+}
+
+export const usernameRule = `❌ وضع الاسم واللقب واليوزرنيم (المعرف).`
+
+export const askTrustRule = `❌ التوثيقات تكون في مجموعة التوثيق والنقاشات: https://t.me/+1dS6r91r0hYzZWZk`
+
+export const reserveRule = `❌ يجب أن يكون الحجز ردًا على العرض نفسه.`
+
+export const formatRule = `❌ احترام النموذج (اضغط على النص 👇 لنسخه):
+  <code>
+  🟢 شراء / 🔴 بيع
+  ❶☚ اسم العملة: 
+  ❷☚ الكمية: 
+  ❸☚ السعر: 
+  ➍☚ طرق الدفع: 
+  الملاحظة: </code>`
